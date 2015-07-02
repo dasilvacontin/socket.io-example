e733eb9b1ae4796be8e68682ab839e08c61f5469
David Ramirez <endaramiz@gmail.com>
2015-07-02 12:56:59 +0200

    carrots: log de colisiones

--- server/index.js
+++ server/index.js
@@ -1,6 +1,9 @@
 var port = process.env.PORT || 9000
 var io = require('socket.io')(port)
 
+var CarrotRadius = 25
+var PlayersRadius = 64
+
 var players = {}
 var carrots = {}
 
@@ -27,8 +30,23 @@ io.on('connection', function (socket) {
     pos.id = socket.id
     players[socket.id] = pos
     socket.broadcast.emit('update_position', pos)
+    
+    //comprobar colisiones
+    for (var carrotID in carrots) {
+      var carrotPos = carrots[carrotID]
+      if (dist(carrotPos, pos) < CarrotRadius + PlayersRadius)
+        console.log(socket.id+': Numnumnum!')
+	}
   })
 })
+
+function dist(pos1, pos2) {
+	var posN = {
+		w: pos2.x - pos1.y,
+		h: pos2.y - pos1.y
+	}
+	return Math.sqrt(posN.w*posN.w + posN.h*posN.h)
+}
 
 // INIT Pastanagues
 var numCarrots = 3
